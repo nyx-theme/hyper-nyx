7fdf38dc7c943a909a24e5721418d1bd733b54f9
Ulyso <i@ulyso.me>
2018-09-18 22:36:40 +0800

    fix code style

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
-const foregroundColor = '#F0F3F5'
-const backgroundColor = '#282A36'
-const black = '#44475a'
-const red = '#FF3B30'
-const green = '#4CD964'
-const yellow = '#F8E71C'
-const blue = '#3672F8'
-const pink = '#E1467C'
-const turquoise = '#14F1D9'
-const gray = '#CCCCCC'
-const brightBlack = '#999999'
-const brightWhite = '#ffffff'
-const purple = '#7B42F6'
+const foregroundColor = "#F0F3F5";
+const backgroundColor = "#282A36";
+const black = "#44475a";
+const red = "#FF3B30";
+const green = "#4CD964";
+const yellow = "#F8E71C";
+const blue = "#3672F8";
+const pink = "#E1467C";
+const turquoise = "#14F1D9";
+const gray = "#CCCCCC";
+const brightBlack = "#999999";
+const brightWhite = "#ffffff";
+const purple = "#7B42F6";
 
 exports.decorateConfig = config => {
     return Object.assign({}, config, {
@@ -35,17 +35,10 @@ exports.decorateConfig = config => {
 
             // bright
             brightBlack,
-            red,
-            green,
-            yellow,
-            blue,
-            pink,
-            turquoise,
-            brightWhite,
-            purple
+            brightWhite
         },
         css: `
-        ${config.css || ''}
+        ${config.css || ""}
             .tabs_list .tab_tab.tab_active .tab_text  {
                 background: ${backgroundColor};
             }
@@ -53,5 +46,5 @@ exports.decorateConfig = config => {
                 border-color: rgb(68, 71, 90);
             }
         `
-    })
-}
+    });
+};
